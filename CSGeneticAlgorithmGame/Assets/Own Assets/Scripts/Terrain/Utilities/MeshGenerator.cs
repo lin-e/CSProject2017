a50438924f92b2b7d6290babe3956a4028c329ed
lin-e <lightning@live.com>
2018-03-02 23:19:30 +0000

    Commented mesh generator

--- CSGeneticAlgorithmGame/Assets/Own Assets/Scripts/Terrain/Utilities/MeshGenerator.cs
+++ CSGeneticAlgorithmGame/Assets/Own Assets/Scripts/Terrain/Utilities/MeshGenerator.cs
@@ -5,55 +5,54 @@ public static class MeshGenerator
 {
     public static MeshData GenerateMesh(float[,] map)
     {
-        // SEBASTIAN LAGUE
-        int width = map.GetLength(0);
-        int height = map.GetLength(1);
-        float[] offset = { (width - 1) / -2f, (height - 1) / 2f };
-        MeshData data = new MeshData();
-        int v = 0;
-        for (int y = 0; y < height; y++)
+        int width = map.GetLength(0); // get the width of the heightmap
+        int height = map.GetLength(1); // get the height
+        float[] offset = { (width - 1) / -2f, (height - 1) / 2f }; // calculate the offsets
+        MeshData data = new MeshData(); // create a new empty mesh
+        int v = 0; // vert counter
+        for (int y = 0; y < height; y++) // iterate through each row
         {
-            for (int x = 0; x < width; x++)
+            for (int x = 0; x < width; x++) // iterate through each column
             {
-                data.AddVertex(offset[0] + x, map[x, y], offset[1] - y);
-                if ((x < (width - 1)) && (y < (height - 1)))
+                data.AddVertex(offset[0] + x, map[x, y], offset[1] - y); // add vertex based on offset
+                if ((x < (width - 1)) && (y < (height - 1))) // small check
                 {
-                    data.AddTriangle(v, v + width + 1, v + width);
-                    data.AddTriangle(v + width + 1, v, v + 1);
+                    data.AddTriangle(v, v + width + 1, v + width); // add first triangle
+                    data.AddTriangle(v + width + 1, v, v + 1); // add second one
                 }
-                v++;
+                v++; // increment verts
             }
         }
-        return data;
+        return data; // return generated data
     }
 }
 public class MeshData
 {
     List<Vector3> vertices;
     List<int> triangles;
-    public MeshData()
+    public MeshData() // constructor
     {
-        vertices = new List<Vector3>();
+        vertices = new List<Vector3>(); // create list
         triangles = new List<int>();
     }
-    public void AddTriangle(params int[] p)
+    public void AddTriangle(params int[] p) // add triangle
     {
-        foreach (int point in p)
+        foreach (int point in p) // iterate points
         {
-            triangles.Add(point);
+            triangles.Add(point); // add to triangle
         }
     }
-    public void AddVertex(float x, float y, float z)
+    public void AddVertex(float x, float y, float z) // simple function to add vertices
     {
-        vertices.Add(new Vector3(x, y, z));
+        vertices.Add(new Vector3(x, y, z)); // add new vert
     }
-    public Mesh Generate()
+    public Mesh Generate() // export mesh
     {
-        Mesh ret = new Mesh();
-        ret.vertices = vertices.ToArray();
-        ret.triangles = triangles.ToArray();
-        ret.RecalculateNormals();
-        ret.RecalculateBounds();
-        return ret;
+        Mesh ret = new Mesh(); // create new return mesh
+        ret.vertices = vertices.ToArray(); // convert vertices to array to array
+        ret.triangles = triangles.ToArray(); // same for triangles
+        ret.RecalculateNormals(); // calculate render normals
+        ret.RecalculateBounds(); // calculate collider bounds
+        return ret; // return generated mesh
     }
 }
