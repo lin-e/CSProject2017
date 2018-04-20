eceb90aa03f980df3c26cb869aafaa5ec51bc0bc
lin-e <me@eugenel.in>
2018-04-21 00:20:35 +0200

    Created webserver directory

---
+++ Website/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>"COMING SOON&trade;"</title>
+  </head>
+  <body>
+    <div style="display: flex; justify-content: center; align-items: center;">
+      <pre>"PLACEHOLDER TEXT"</pre>
+    </div>
+  </body>
+</html>
