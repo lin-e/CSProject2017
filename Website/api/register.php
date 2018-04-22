4b614d801cb06e1e1a675c826e01d5100b4d8a7f
lin-e <me@eugenel.in>
2018-04-22 18:57:04 +0100

    Added username constraints

--- Website/api/register.php
+++ Website/api/register.php
@@ -3,6 +3,24 @@
   $data = json_decode($_POST['data']); // take the post parameter and decode it
   if (!isset($data->username)) { // if the username isn't set in the json
     die("{\"status\":0,\"content\":\"No username specified\"}"); // die with error
+  } else {
+    $username = strtolower($data->username);
+    if (strlen($username) < $username_min_length || strlen($username) > $username_max_length) { // check username length
+      die("{\"status\":0,\"content\":\"Username must be between ".str($username_min_length)." and ".str($username_max_length)." characters\"}");
+    } else {
+      $allowed_chars = $username_allowed_chars; // pull allowed characters from config
+      $valid = true; // flag for name validity
+      for ($i = 0; $i < strlen($username); $i++) { // iterate through each character in string
+        $char = strval($username[$i]); // get character as string
+        if (!(strpos($allowed_chars, $char) !== false)) { // check if not contained in allowed characters
+          $valid = false; // mark as false
+          break; // exit loop
+        }
+        if (!$valid) { // if invalid
+          die("{\"status\":0,\"content\":\"Invalid characters in username; only alphanumeric characters, dots, dashes and underscores permitted.\"}"); // die with error
+        }
+      }
+    }
   }
   if (!isset($data->md5)) { // if the hash isn't set
     die("{\"status\":0,\"content\":\"No password hash specified\"}");
--- Website/assets/includes/config.php
+++ Website/assets/includes/config.php
@@ -6,4 +6,8 @@
   $db_hostname = 'localhost'; // MySQL host
   $db_name = 'cs_project'; // MySQL datbase name
   $db = mysqli_connect($db_hostname, $db_username, $db_password, $db_name) or die("{\"status\":0,\"content\":\"Failed to connect to database\"}"); // connect or error
+
+  $username_min_length = 4;
+  $username_max_length = 32;
+  $username_allowed_chars = 'abcdefghijklmnopqrstuvwxyz0123456789_-.';
 ?>
