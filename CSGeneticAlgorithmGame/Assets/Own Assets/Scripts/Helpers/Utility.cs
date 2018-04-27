17aac190b2df45518532537d2fee07c5320258d6
lin-e <lightning@live.com>
2018-04-27 21:42:22 +0100

    Added login functions

--- CSGeneticAlgorithmGame/Assets/Own Assets/Scripts/Helpers/Utility.cs
+++ CSGeneticAlgorithmGame/Assets/Own Assets/Scripts/Helpers/Utility.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using System.Security.Cryptography;
+using System.Text;
 using UnityEngine;
 
 public static class Utility // class for random actions i might need to reuse
@@ -12,5 +14,38 @@ public static class Utility // class for random actions i might need to reuse
     {
         yield return new WaitForSecondsRealtime(time); // wait for the specified time
         callback(); // do the callback
+    }
+    public static string ToHex(this byte[] input) // converts a byte array to a hex string
+    {
+        StringBuilder builder = new StringBuilder(input.Length * 2); // create a string builder with double the length of the array (each byte takes 2 hex characters)
+        foreach (byte b in input) // iterates through each byte
+        {
+            builder.Append(b.ToString("x2")); // append the byte converted to lowercase hex to the builder
+        }
+        return builder.ToString(); // return the builder's final string
+    }
+    public static string Hash(this string inputString, string hashAlg) // hash a string, has support for basic types
+    {
+        byte[] bytes = Encoding.UTF8.GetBytes(inputString); // gets utf bytes of input
+        switch (hashAlg.ToLower()) // switches depending on the selected algorithm
+        {
+            case "md5": // if it's md5
+                {
+                    return new MD5CryptoServiceProvider().ComputeHash(bytes).ToHex(); // compute with md5 provider
+                }
+            case "sha1": // same with sha1
+                {
+                    return new SHA1CryptoServiceProvider().ComputeHash(bytes).ToHex();
+                }
+            case "sha256": // these two refer to the same algorithm hence the fallthrough
+            case "sha2":
+                {
+                    return new SHA256CryptoServiceProvider().ComputeHash(bytes).ToHex();
+                }
+            default: // if any other algorithm is specified
+                {
+                    return ""; // return an empty string
+                }
+        }
     }
 }
---
+++ CSGeneticAlgorithmGame/Assets/Own Assets/Scripts/Managers/SessionManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.IO;
+using System.Text;
+
+public static class SessionManager // by using a static class we have variables that can be accessed from all scenes
+{
+    public static string EndPoint = "https://project.eugenel.in/api/";
+    public static WebRequestFactory Factory = new WebRequestFactory(null, new CookieContainer()); // create a new request manager
+    public static string Token = ""; // stores the current active token
+    public static dynamic Authenticate(string user, string pass) // method to authenticate the user
+    {
+        try // attempt to run the code in the child scope
+        {
+            string payload = "data=" + WebUtility.UrlEncode("{\"username\":\"" + user + "\",\"md5\":\"" + pass.Hash("md5") + "\"}"); // encode the payload for upload
+            HttpWebRequest auth = Factory.WebRequestPOST( // create a new request from the factory
+                EndPoint + "auth.php", // set the url to be auth.php
+                new string[][] { }, // no headers needed
+                Encoding.ASCII.GetBytes(payload), // convert the payload to bytes
+                "application/x-www-form-urlencoded" // specify that it is a form (with json payload)
+                );
+            using (HttpWebResponse response = (HttpWebResponse)auth.GetResponse()) // automatic disposal
+            {
+                dynamic result = DynamicJson.Deserialize(new StreamReader(response.GetResponseStream()).ReadToEnd()); // deserialize the string after reading it from stream
+                if ((int)result.status == 1) // if the request was successful
+                {
+                    Token = (string)result.content; // set the token to be the response
+                }
+                return result; // return the decoded content
+            }
+        }
+        catch // if it errors out (most likely due to connection)
+        {
+            return DynamicJson.Deserialize("{\"status\":0,\"content\":\"Error connecting to server\"}"); // return a generic error
+        }
+    }
+}
