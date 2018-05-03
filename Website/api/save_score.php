3c7cfcd6cf96de916a103fe0d90e5c1ef6bff71a
lin-e <me@eugenel.in>
2018-05-03 21:30:38 +0100

    Prevented token reuse

--- Website/api/save_score.php
+++ Website/api/save_score.php
@@ -4,16 +4,16 @@
   if (!isset($data->token)) { // if token is empty
     die("{\"status\":0,\"content\":\"Please provide an authorisation token\"}"); // error for authorisation
   } else {
-    if (!isset($data->score)) {
-      die("{\"status\":0,\"content\":\"Please provide a score\"}");
-      if (!is_int($data->score)) {
-        die("{\"status\":0,\"content\":\"Please provide a valid score\"}");
-      }
-    }
     $token = $data->token; // extract the token from the input json
     $result = validate_token($token); // validate the token from the master function
     if ($result["status"] == 1) { // if the result is a success
       $new_token = $result["content"]["token"]; // create a new token
+      if (!isset($data->score)) { // if the score isn't set
+        die("{\"status\":0,\"content\":\"{\"token\":\"$new_token\",\"body\":\"Please provide a score\"}\"}"); // die with token
+        if (!is_int($data->score)) { // if the token is invalid
+          die("{\"status\":0,\"content\":\"{\"token\":\"$new_token\",\"body\":\"Please provide a valid score\"}\"}"); // die with token
+        }
+      }
       $score = strval(intval($data->score)); // get the score as a string
       $time = strval(time()); // get the current time as a string
       $username = $result["content"]["username"]; // fetch the username
