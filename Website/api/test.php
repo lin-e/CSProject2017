208ff0dd763f210406822e0f2adac5b4e3e6e89e
lin-e <me@eugenel.in>
2018-05-05 13:55:31 +0100

    More db tests

--- Website/api/test.php
+++ Website/api/test.php
@@ -2,7 +2,8 @@
   include("../assets/includes/master.php"); // include configuration (which includes database connection)
   $conn = new PDO("mysql:host=$db_hostname;dbname=$db_name", $db_username, $db_password);
   $result = $conn->prepare("SELECT * FROM users WHERE username= :username");
-  $result->bindParam(':username', 'authtest');
+  $uname = 'authtest';
+  $result->bindParam(':username', $uname);
   $result->execute();
   var_dump($result->fetch(PDO::FETCH_ASSOC));
 ?>
