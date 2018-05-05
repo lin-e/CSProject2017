df1cd71f7519c094cf94f5cc4fac0706c9d4d50a
lin-e <me@eugenel.in>
2018-05-05 13:48:38 +0100

    Testing stuff, not project related

--- Website/api/test.php
+++ Website/api/test.php
@@ -1,18 +1,7 @@
 <?php
   include("../assets/includes/master.php"); // include configuration (which includes database connection)
-  $data = json_decode($_POST['data']); // take the post parameter and decode it
-  if (!isset($data->token)) { // if captcha is empty
-    die("{\"status\":0,\"content\":\"Please provide an authorisation token\"}");
-  } else {
-    $token = $data->token;
-    $result = validate_token($token);
-    if ($result["status"] == 1) {
-      $new_token = $result["content"];
-      die("{\"status\":1,\"content\":\"$new_token\"}");
-    } else {
-      $content = $result["content"];
-      die("{\"status\":0,\"content\":\"$content\"}");
-    }
-  }
-  die("{\"status\":0,\"content\":\"Unknown error\"}");
+  $result = $db->prepare("SELECT * FROM users WHERE username= :username");
+  $result->bindParam(':username', 'authtest');
+  $result->execute();
+  var_dump($result->fetch(PDO::FETCH_ASSOC));
 ?>
